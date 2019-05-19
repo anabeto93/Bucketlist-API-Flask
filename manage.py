5550adaabc40ef8c8cf8d90cca89f685e5a2462d
Richard Anabeto Opoku <anabeto93@gmail.com>
2019-05-19 00:08:01 +0000

    responsible for running migrations everytime a model is created or modified

---
+++ manage.py
@@ -0,0 +1,20 @@
+import os
+
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+from app import db, create_app
+from models.bucket import BucketList
+
+app = create_app(config_name=os.getenv('APP_SETTINGS','development'))
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
+
+
+
