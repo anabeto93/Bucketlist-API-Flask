14fcbf51acc0da9a2b748e997977da192ea9b953
Richard Anabeto Opoku <anabeto93@gmail.com>
2019-05-19 02:01:03 +0000

    POST endpoint to create bucketlist ready

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,9 @@ from instance.config import app_config
 # initialize sql-alchemy
 db = SQLAlchemy()
 
+#imports to now build functionality
+from flask import request, jsonify, abort
+
 def create_app(config_name):
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
@@ -15,6 +18,30 @@ def create_app(config_name):
 
     db.init_app(app)
 
-    
+    #import the models
+    from models.bucket import BucketList
+
+    #get and create a bucketlist
+    @app.route('/bucketlists/', methods=['POST', 'GET'])
+    def bucketlists():
+        if request.method == "POST":
+            name = str(request.data.get('name', '')) # return empty by default
+            if name:
+                b_list = BucketList(name=name)
+                b_list.save()
+
+                #return the response
+                response = jsonify({
+                    'id': b_list.id,
+                    'name': b_list.name,
+                    'created_at': b_list.date_created,
+                    'updated_at': b_list.date_modified
+                })
+                response.status_code = 201
+
+                return response
+        else:
+            # GET request by default
+            pass
 
     return app
